10bc7c35e647f678b9c8c1585222992826e2894f
“[firstname <singhdevendra8398@gmail.com>
2024-03-06 18:51:57 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ## flask app routing
-from flask import Flask
+from flask import Flask,render_template,request,url_for,redirect
 
 
 ##create simple flask application
@@ -7,12 +7,41 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def welcome():
-    return "welcom to the devendra github"
+    return "<h1>welcom to the devendra github<h1>"
 
 @app.route("/index",methods=["GET"])
 def indexpage():
-    return "welcom to the index page"
+    return "<h2>welcom to the index page<h2>"
 
+## variable rule
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person has passed and the score is" + str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has fail and the score is" + str(score)
+
+@app.route("/form",methods=["GET","POST"])
+def form():
+    if request.method=="GET":
+        return render_template("form.html")
+    else:
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+
+        average_marks=(maths+science+history)/3
+
+        res=""
+        if average_marks>=50:
+            res="success"
+        else:
+            res="fail"
+        return redirect(url_for(res,score=average_marks))       
+        
+
+        ##return render_template('form.html',score=average_marks)
 
 
 
